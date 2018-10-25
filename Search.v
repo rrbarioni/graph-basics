7436e56d62fa1a08076dafead0aaa912949f9fdb
rrb <rrb@cin.ufpe.br>
2018-10-25 15:42:40 -0300

    Testing AdjList

--- Search.v
+++ Search.v
@@ -34,6 +34,30 @@ Fixpoint Graph_to_AdjList (v : V_set) (a : A_set) (g : Graph v a) {struct g} :
  AdjList :=
   A_list_to_AdjList (GA_list v a g) AL_empty.
 
-Compute (Graph_to_AdjList V_empty A_empty G_empty).
+Definition my_V_list :=
+  V_union
+  (
+    V_union
+      (V_single (index 1))
+      (V_single (index 2))
+  )
+  (
+    V_single (index 3)
+  ).
+
+Definition my_A_list :=
+  A_union
+  (
+    A_union
+      (A_single (A_ends (index 1) (index 2)))
+      (A_single (A_ends (index 2) (index 3)))
+  )
+  (
+    A_single (A_ends (index 1) (index 3))
+  ).
+
+Definition my_G := Graph my_V_list my_A_list.
+
+Compute (Graph_to_AdjList my_V_list my_A_list Graph).
 
 End SEARCH.
